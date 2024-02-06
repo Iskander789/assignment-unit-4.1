8b59d8cd500ee2926685d2a5a9bb247493de8b3a
Sean Harrison <sharrison9@gmail.com>
2024-02-06 15:41:28 -0600

    passing no. 6

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -53,8 +53,13 @@ console.log(isPositive(0));
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[array.length - 1];
 }
+
+console.log(getLast([1, 2, 3]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
